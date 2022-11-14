91fa53132e238d8ed38550b7c00e31dffb20c71e
Thomas Bonnardot <thomas.bonnardot@hei.fr>
2022-11-14 15:21:24 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, Form } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world !
+        </p>
+        <Form >
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <div className="d-grid gap-2">
+
+      <Button variant="primary"  type="submit">
+        Submit
+      </Button>
+      </div>
+    </Form>
+        </header>
+
+    </div>
+  );
+}
+
+export default App;
